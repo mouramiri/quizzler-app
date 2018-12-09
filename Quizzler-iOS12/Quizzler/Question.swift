15078d4b8858300acf7f735d67de852d8a1e7b1d
Miriana Moura <mouramiri@outlook.com>
2018-12-09 17:42:20 -0500

    Lesson 9
    Part of lesson 9 (covered OOP, HUD, refactoring, debugging class initialization and design patterns.

---
+++ Quizzler-iOS12/Quizzler/Question.swift
@@ -0,0 +1,27 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Miriana Moura on 12/8/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+
+//Properties
+    let questionText: String
+    let answer: Bool
+    
+//Events
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+
+//Actions
+//    func someFunction() {
+//        //some code to run
+//    }
+}
